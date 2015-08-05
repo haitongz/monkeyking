273c3c53ac99384a879f57b9712a70f3d8a91aad
pengxu7 <pengxu7@gmail.com>
2015-08-05 11:36:01 -0400

    Create circular_move.cpp

---
+++ circular_move.cpp
@@ -0,0 +1,41 @@
+/*
+Given a sequence of moves for a robot, check if the sequence is circular or not. A sequence of moves is circular if first and last positions of robot are same. A move can be on of the following.
+
+  G - Go one unit
+  L - Turn left
+  R - Turn right
+ */
+#include <iostream>
+
+using namespace std;
+
+bool isCircular(char path[]) {
+  // Initialize starting point for robot as (0, 0) and starting direction as N North
+  uint32_t x = 0, y = 0;
+  uint32_t dir = N;
+
+  for (uint32_t i = 0; path[i]; ++i) {
+    char move = path[i];
+    if (move == 'R') // If move is left or right, then change direction
+      dir = (dir+1)%4;
+    else if (move == 'L')
+      dir = (4+dir-1)%4;
+    else { // if (move == 'G')
+      // If move is Go, then change x or y according to current direction
+      if (dir == N)
+        ++y;
+      else if (dir == E)
+        ++x;
+      else if (dir == S)
+        --y;
+      else // dir == W
+        --x;
+    }
+  }
+
+  // If robot comes back to (0, 0), then path is cyclic
+  return (x == 0 && y == 0);
+}
+
+int main(int argc, char** argv) {
+}
