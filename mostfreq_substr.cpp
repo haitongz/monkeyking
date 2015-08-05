006e8180c7375d020605138e71902aadcbac88d0
pengxu7 <pengxu7@gmail.com>
2015-08-05 11:48:30 -0400

    Create mostfreq_substr.cpp

---
+++ mostfreq_substr.cpp
@@ -0,0 +1,11 @@
+/*
+Given a string, find most frequent substring.
+For example, input "This is island." output "is"
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  return 0;
+}
