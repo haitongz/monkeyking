50c73de34e711969bf2c29de096896cac30342c2
pengxu7 <pengxu7@gmail.com>
2015-08-05 11:26:20 -0400

    Create word_break.cpp

---
+++ word_break.cpp
@@ -0,0 +1,43 @@
+/*
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+ */
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+bool wordbreak(const string& s, const unordered_set<string>& dict) {
+  const uint32_t len = s.length();
+  if (len <= 1) {
+    return dict.count(s) ? true : false;
+  }
+
+  bool dp[len+1];
+  dp[0] = true;
+
+  for (uint32_t i = 0; i < len; ++i) {
+    for (int32_t j = i, k = 1; j >= 0; ++k, --j) {
+      string j_sub = s.substr(j, k);
+      if (dp[j] && dict.count(j_sub)) {
+        dp[i+1] = true;
+        break;
+      }
+    }
+  }
+
+  return dp[len];
+}
+
+int main() {
+  unordered_set<string> dict;
+  dict.insert("leet");
+  dict.insert("code");
+
+  cout << "String 'leetcode' can " << (wordbreak("leetcode", dict) ? "" : "not ") << "be segmented!" << endl;
+}
