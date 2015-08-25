363e1747811e7ae92ea55a0756807d5fd27c4230
pengxu7 <pengxu7@gmail.com>
2015-08-25 11:53:19 -0400

    Update with_five.cpp

--- with_five.cpp
+++ with_five.cpp
@@ -9,30 +9,30 @@ Write a function to ouput all numbers with 5 in it. func(60) output is 5,15,25,3
 
 using namespace std;
 
-vector<uint32_t> numbersWith5(const uint32_t n) {
+vector<uint32_t> numsWith5(const uint32_t n) {
   if (n < 5)
     return {};
 
-  ostringstream oss;
-  oss << n;
-  const string number = oss.str();
+  char s[32];
+  itoa(n, s);
+  const string number(s);
   const uint32_t len = number.length();
   string to_ext = "";
-  vector<uint32_t> res;
+  vector<uint32_t> ret;
 
-  function<void(const uint32_t,const bool)> backtrack =
+  function<void(const uint32_t,const bool)> solve = // backtracking
     [&](const uint32_t idx, const bool there_is_5) {
     if (idx == len) {
-      res.push_back(atoi(to_ext.c_str()));
+      ret.push_back(atoi(to_ext.c_str()));
       return;
     } else if (idx < len-1 || there_is_5) {
       for (uint8_t i = 0; i < 10; ++i) {
         char buf[2]; // notice: use extra byte for '\0'
-        itoa(i, buf, 10);
+        itoa(i, buf);
         to_ext += string(buf);
         string new_num = number.substr(0, idx+1);
         if (atoi(to_ext.c_str()) <= atoi(new_num.c_str())) {
-          backtrack(idx+1, (i == 5) ? true : there_is_5);
+          solve(idx+1, (i == 5) ? true : there_is_5);
         }
         to_ext.pop_back();
       }
@@ -40,19 +40,18 @@ vector<uint32_t> numbersWith5(const uint32_t n) {
       to_ext += '5';
       const uint32_t new_num = atoi(to_ext.c_str());
       if (new_num < n) // need to be less than n
-        res.push_back(new_num);
+        ret.push_back(new_num);
       to_ext.pop_back();
     }
   };
 
-  backtrack(0, false);
-  return res;
+  solve(0, false);
+  return ret;
 }
 
 int main(int argc, char** argv) {
   const uint32_t n = 1000;
-  vector<uint32_t> res = numbersWith5(n);
-
+  vector<uint32_t> res = numsWith5(n);
   for (auto i : res)
     cout << i << " ";
   cout << endl;
