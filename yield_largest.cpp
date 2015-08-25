6df73412c1c6f33be04b1519f1dcccf06f964cdd
pengxu7 <pengxu7@gmail.com>
2015-08-25 10:02:39 -0400

    Update yield_largest.cpp

--- yield_largest.cpp
+++ yield_largest.cpp
@@ -4,28 +4,3 @@ Given an array of numbers, arrange them in a way that yields the largest value. 
 {1, 34, 3, 98, 9, 76, 45, 4}, then the arrangement 998764543431 gives the largest value.
  */
 #include <iostream>
-
-int myCompare(string X, string Y)
-{
-    // first append Y at the end of X
-    string XY = X.append(Y);
- 
-    // then append X at the end of Y
-    string YX = Y.append(X);
- 
-    // Now see which of the two formed numbers is greater
-    return XY.compare(YX) > 0 ? 1: 0;
-}
- 
-// The main function that prints the arrangement with the largest value.
-// The function accepts a vector of strings
-void printLargest(vector<string> arr)
-{
-    // Sort the numbers using library sort funtion. The function uses
-    // our comparison function myCompare() to compare two strings.
-    // See http://www.cplusplus.com/reference/algorithm/sort/ for details
-    sort(arr.begin(), arr.end(), myCompare);
- 
-    for (int i=0; i < arr.size() ; i++ )
-        cout << arr[i];
-}
