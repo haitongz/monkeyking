d0ee4128b5b942c6a871e79981acc599485f8421
pengxu7 <pengxu7@gmail.com>
2015-08-26 12:45:53 -0400

    Update unique_paths.cpp

--- unique_paths.cpp
+++ unique_paths.cpp
@@ -1,5 +1,6 @@
-/**
-Imagine a robot sitting on the upper left corner of an X by Y grid. The robot can only move in two directions: right and down. How many possible paths are there for the robot to go from (0, 0) to (X, Y)?
+/*
+Imagine a robot sitting on the upper left corner of an X by Y grid. The robot can only move in two directions:
+right and down. How many possible paths are there for the robot to go from (0, 0) to (X, Y)?
 
 FOLLOW UP: Imagine certain spots are "off limits", such that the robot cannot step on them.
  */
