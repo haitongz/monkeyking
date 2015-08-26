043689efc213ce068234ef9dd6bc045752c2b503
pengxu7 <pengxu7@gmail.com>
2015-08-26 11:10:38 -0400

    Update wave_sort.cpp

--- wave_sort.cpp
+++ wave_sort.cpp
@@ -1,12 +1,15 @@
 /*
-Given an unsorted array of integers, sort the array into a wave like array. An array a[0..n-1] is sorted in wave form if arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= ...
+Given an unsorted array of integers, sort the array into a wave like array. An array a[0..n-1] is sorted in wave form
+if a[0] >= a[1] <= a[2] >= a[3] <= a[4] >= ...
  */
 #include <iostream>
 
 using namespace std;
 
 /*
-This can be done in O(n) time by doing a single traversal of given array. The idea is based on the fact that if we make sure that all even positioned (at index 0, 2, 4, ..) elements are greater than their adjacent odd elements, we don't need to worry about odd positioned element. Following are simple steps.
+This can be done in O(n) time by doing a single traversal of given array. The idea is based on the fact that if we make
+sure that all even positioned (at index 0, 2, 4, ..) elements are greater than their adjacent odd elements, we don't need
+to worry about odd positioned element. Following are simple steps.
 1) Traverse all even positioned elements of input array, and do following.
 ...a) If current element is smaller than previous odd element, swap previous and current.
 ...b) If current element is smaller than next odd element, swap next and current.
