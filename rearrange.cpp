bb8618bd404f51cdf04413f14158b402ecf1e746
pengxu7 <pengxu7@gmail.com>
2015-08-27 10:02:11 -0400

    Update rearrange.cpp

--- rearrange.cpp
+++ rearrange.cpp
@@ -1,8 +1,12 @@
 /*
-An array contains both positive and negative numbers in random order. Rearrange the array elements so that positive and negative numbers are placed alternatively. Number of positive and negative numbers need not be equal. If there are more positive numbers they appear at the end of the array. If there are more negative numbers, they too appear in the end of the array.
+An array contains both positive and negative numbers in random order. Rearrange the array elements so that positive and
+negative numbers are placed alternatively. Number of positive and negative numbers need not be equal. If there are more
+positive numbers they appear at the end of the array. If there are more negative numbers, they too appear in the end of
+the array.
  */
 void rearrange(int32_t a[], const uint32_t n) {
-  // The following few lines are similar to partition process of QuickSort. The idea is to consider 0 as pivot and divide the array around it.
+  // The following few lines are similar to partition process of QuickSort. The idea is to consider 0 as pivot and divide
+  // the array around it.
   int32_t i = -1;
   for (uint32_t j = 0; j < n; j++) {
     if (a[j] < 0) {
