7ad4ae91700416cb8fa90f13784af2cbdf39625b
pengxu7 <pengxu7@gmail.com>
2015-08-31 14:42:36 -0400

    Update buy_sell.cpp

--- buy_sell.cpp
+++ buy_sell.cpp
@@ -10,7 +10,7 @@ Say you have an array for which the ith element is the price of a given stock on
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
 design an algorithm to find the maximum profit.
  */
-double maxProfit(const vector<double>& prices) {
+double maxProfit_1trans(const vector<double>& prices) {
   const uint32_t n = prices.size();
   if (n < 2) {
 #ifdef _DEBUG_
@@ -20,18 +20,18 @@ double maxProfit(const vector<double>& prices) {
   }
 
   double lowest_px = prices[0];
-  double res = 0.0;
+  double ret = 0.0;
 
   for (uint32_t i = 1; i < n; ++i) { // time series when scanning from first to last
     if (prices[i] < lowest_px) {
       lowest_px = prices[i]; // no need to check max_profit because you have to buy first sell later
     } else {
-      const double maxpft_sellati = prices[i]-lowest_px;
-      res = max(res, maxpft_sellati);
+      const double sellati_pft = prices[i]-lowest_px;
+      ret = max(ret, sellati_pft);
     }
   }
 
-  return res;
+  return ret;
 }
 
 /*
@@ -41,7 +41,7 @@ Design an algorithm to find the maximum profit. You may complete as many transac
 (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions
 at the same time (ie, you must sell the stock before you buy again).
  */
-double maxProfit2(const vector<double>& prices) {
+double maxProfit_anytrans(const vector<double>& prices) {
   const uint32_t n = prices.size();
   if (n < 2) {
 #ifdef _DEBUG_
@@ -50,12 +50,12 @@ double maxProfit2(const vector<double>& prices) {
     return 0.0;
   }
 
-  double res = 0.0;
+  double ret = 0.0;
   for (uint32_t i = 1; i < n; ++i) {
-    res += max(prices[i]-prices[i-1], 0.0); // logic: price 1,2,3, profit is same between (3-1) and (2-1+3-2)
+    ret += max(prices[i]-prices[i-1], 0.0); // logic: price 1,2,3, profit is same between (3-1) and (2-1+3-2)
   }
 
-  return res;
+  return ret;
 }
 
 /*
@@ -90,7 +90,7 @@ double maxProfit3(const vector<double>& prices) {
   return dp[0];
 }
 */
-double maxProfit3(const vector<double>& prices) {
+double maxProfit_2trans(const vector<double>& prices) {
   const uint32_t n = prices.size();
   if (n < 2)
     return 0.0; //throw excpetion();
@@ -115,7 +115,7 @@ Say you have an array for which the ith element is the price of a given stock on
 
 Design an algorithm to find the maximum profit. You may complete at most k transactions.
  */
-double maxProfit4(const vector<double>& prices, uint32_t k) {
+double maxProfit_ktrans(const vector<double>& prices, uint32_t k) {
   const uint32_t n = prices.size();
   if (n < 2 || k < 1)
     return 0.0;
@@ -135,7 +135,7 @@ double maxProfit4(const vector<double>& prices, uint32_t k) {
 
     for (uint32_t j = 1; j <= k; ++j) {
       local[i][j] = max(global[i-1][j-1]+max(diff, 0.0), local[i-1][j]+diff); // There are total j transactions made till day
-      // i. Two different senarios: 1. jth transaction (buy and sell) takes place on day i; 2. a position was bought earlier
+      // i. Two different senarios: 1. j-th transaction (buy and sell) takes place on day i; 2. a position was bought earlier
       // but sold on day i. the profit is equal to profit on day i-1 plus stock value change on day i.
 
       global[i][j] = max(local[i][j], global[i-1][j]);
@@ -146,31 +146,38 @@ double maxProfit4(const vector<double>& prices, uint32_t k) {
 }
 
 /*
-The advice to "buy low" is half the formula to success in the bovine stock market. To be considered a great investor you must also follow this problems' advice: "Buy low; buy lower"
+The advice to "buy low" is half the formula to success in the bovine stock market. To be considered a great investor
+you must also follow this problems' advice: "Buy low; buy lower".
+Each time you buy a stock, you must purchase it at a lower price than the previous time you bought it. The more times
+you buy at a lower price than before, the better! Your goal is to see how many times you can continue purchasing at ever
+lower prices.
 
-Each time you buy a stock, you must purchase it at a lower price than the previous time you bought it. The more times you buy at a lower price than before, the better! Your goal is to see how many times you can continue purchasing at ever lower prices.
-
-You will be given the daily selling prices of a stock (positive 16-bit integers) over a period of time. You can choose to buy stock on any of the days. Each time you choose to buy, the price must be strictly lower than the previous time you bought stock. Write a program which identifies which days you should buy stock in order to maximize the number of times you buy.
+You will be given the daily selling prices of a stock (positive 16-bit integers) over a period of time. You can choose to buy
+stock on any of the days. Each time you choose to buy, the price must be strictly lower than the previous time you bought
+stock. Write a program which identifies which days you should buy stock in order to maximize the number of times you buy.
 
 Here is a list of stock prices:
- Day   1  2  3  4  5  6  7  8  9 10 11 12
-
+Day    1  2  3  4  5  6  7  8  9 10 11 12
 Price 68 69 54 64 68 64 70 67 78 62 98 87
 
-The best investor (by this problem, anyway) can buy at most four times if each purchase is lower then the previous purchase. One four day sequence (there might be others) of acceptable buys is:
+The best investor (by this problem, anyway) can buy at most four times if each purchase is lower then the previous purchase.
+One four day sequence (there might be others) of acceptable buys is:
 Day    2  5  6 10
-
 Price 69 68 64 62
+
 Input
 * Line 1: N (1 <= N <= 5000), the number of days for which stock prices are given
-
 * Lines 2..etc: A series of N space-separated integers, ten per line except the final line which might have fewer integers.
 Output
 Two integers on a single line:
 * The length of the longest sequence of decreasing prices
 * The number of sequences that have this length (guaranteed to fit in 31 bits)
 
-In counting the number of solutions, two potential solutions are considered the same (and would only count as one solution) if they repeat the same string of decreasing prices, that is, if they "look the same" when the successive prices are compared. Thus, two different sequence of "buy" days could produce the same string of decreasing prices and be counted as only a single solution.
+In counting the number of solutions, two potential solutions are considered the same (and would only count as one solution)
+if they repeat the same string of decreasing prices, that is, if they "look the same" when the successive prices are compared.
+Thus, two different sequence of "buy" days could produce the same string of decreasing prices and be counted as only a single
+solution.
+
 Sample Input
 12
 68 69 54 64 68 64 70 67 78 62 98 87
@@ -236,11 +243,11 @@ int main(int argc, char** argv) {
   }*/
 
   vector<double> prices = {10.00,11.50,9.78,15.10,12.48,10.00,13.99,11.72,19.00,18.59};
-  cout << "Max profit with 1 transaction: " << maxProfit(prices) << endl;
-  cout << "Max profit with 1 transaction: " << maxProfit4(prices, 1) << endl;
-  cout << "Max profit with any transactions: " << maxProfit2(prices) << endl;
-  cout << "Max profit with 2 transactions: " << maxProfit3(prices) << endl;
-  cout << "Max profit with 2 transactions: " << maxProfit4(prices, 2) << endl;
+  cout << "Max profit with 1 transaction: " << maxProfit_1trans(prices) << endl;
+  cout << "Max profit with 1 transaction: " << maxProfit_ktrans(prices, 1) << endl;
+  cout << "Max profit with any transactions: " << maxProfit_anytrans(prices) << endl;
+  cout << "Max profit with 2 transactions: " << maxProfit_2trans(prices) << endl;
+  cout << "Max profit with 2 transactions: " << maxProfit_ktrans(prices, 2) << endl;
 
   return 0;
 }
