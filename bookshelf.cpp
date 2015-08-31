c7ee90ede00d4363a7c343c881a4fc974373a0a8
pengxu7 <pengxu7@gmail.com>
2015-08-31 14:07:00 -0400

    Update bookshelf.cpp

--- bookshelf.cpp
+++ bookshelf.cpp
@@ -1,12 +1,18 @@
 /*
-N cows (1 <= N <= 20) each has some height of Hi (1 <= Hi <= 1,000,000 - there are very tall cows). The bookshelf has a height of B (1 <= B <= S, where S is the sum of the heights of all cows).
-To reach the top of the bookshelf, one or more of the cows can stand on top of each other in a stack, so that their total height is the sum of each of their individual heights. This total height must be no less than the height of the bookshelf in order for the cows to reach the top.
-Since a taller stack of cows than necessary can be dangerous, your job is to find the set of cows that produces a stack of the smallest height possible such that the stack can reach the bookshelf. Your program should print the minimal 'excess' height between the optimal stack of cows and the bookshelf.
+N cows (1 <= N <= 20) each has some height of Hi (1 <= Hi <= 1,000,000 - there are very tall cows). The bookshelf has a
+height of B (1 <= B <= S, where S is the sum of the heights of all cows).
+To reach the top of the bookshelf, one or more of the cows can stand on top of each other in a stack, so that their total
+height is the sum of each of their individual heights. This total height must be no less than the height of the bookshelf
+in order for the cows to reach the top.
+Since a taller stack of cows than necessary can be dangerous, your job is to find the set of cows that produces a stack of
+the smallest height possible such that the stack can reach the bookshelf. Your program should print the minimal 'excess'
+height between the optimal stack of cows and the bookshelf.
 Input
 * Line 1: Two space-separated integers: N and B
 * Lines 2..N+1: Line i+1 contains a single integer: Hi
 Output
-* Line 1: A single integer representing the (non-negative) difference between the total height of the optimal set of cows and the height of the shelf.
+* Line 1: A single integer representing the (non-negative) difference between the total height of the optimal set of cows
+and the height of the shelf.
 Sample Input
 5 16
 3
