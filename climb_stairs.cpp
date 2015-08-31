e53bf5e360cb3b8c1df84d31809980ab0e5df7d6
pengxu7 <pengxu7@gmail.com>
2015-08-31 15:28:08 -0400

    Update climb_stairs.cpp

--- climb_stairs.cpp
+++ climb_stairs.cpp
@@ -7,18 +7,17 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 
 using namespace std;
 
-uint32_t climbStairs_iter(const uint32_t n) {
+int32_t climbStairs_iter(const uint32_t n) {
   if (!n)
-    throw exception();
+    return -1; //throw exception();
 
   uint8_t f1 = 1, f2 = 2;
-
   if (n == 1)
     return f1;
   if (n == 2)
     return f2;
-
-  for (uint32_t i = 3; i <= n; ++i) {
+  
+  for (uint32_t i = 3; i < n+1; ++i) {
     uint8_t f3 = f1+f2;
     f1 = f2;
     f2 = f3;
@@ -29,15 +28,15 @@ uint32_t climbStairs_iter(const uint32_t n) {
 
 uint32_t climbStairs_recur(const uint32_t n) {
   function<uint32_t(const uint32_t)> solve =
-    [](const uint32_t num) {
-    if (num <= 0)
+    [&](const uint32_t left) {
+    if (left <= 0)
       return 0;
-    if (num == 1)
+    if (left == 1)
       return 1;
-    if (num == 2)
+    if (left == 2)
       return 2;
 
-    return solve(num-1)+solve(num-2);
+    return solve(left-1)+solve(left-2);
   };
 
   return solve(n);
