745a4da9a9c2594b5fb313c8748f50693eae815f
pengxu7 <pengxu7@gmail.com>
2015-09-03 11:23:48 -0400

    Update three_indices.cpp

--- three_indices.cpp
+++ three_indices.cpp
@@ -1,54 +1,53 @@
 /*
-Given a array of integers, find 3 indices i,j,k such that i<j<k and a[i]<a[j]<a[k]. Best possible is a O(n) algorithm.
+Given an array of integers, find 3 indices i, j and k such that i < j < k and a[i] < a[j] < a[k].
+Best possible is an O(n) algorithm.
  */
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector<int32_t> threeIndices(const vector<int32_t>& nums) {
-  const uint32_t n = nums.size();
+struct Indices {
+  int32_t first, second, third;
+};
+
+Indices threeIndices(const int32_t a[], const uint32_t n) {
   if (n < 3)
     return {};
 
-  vector<int32_t> res = {0,-1,0};
+  Indices ret {0,-1,0};
   int32_t min_idx = 0;
 
   for (uint32_t i = 1; i < n; ++i) {
-    if (res[1] < 0) {
-      if (nums[i] < nums[min_idx]) {
+    if (ret.second < 0) {
+      if (a[i] < a[min_idx]) {
         min_idx = i;
-        res[0] = i;
-      } else if (nums[i] > nums[min_idx]) {
-        res[1] = i;
+        ret.first = i;
+      } else if (a[i] > a[min_idx]) {
+        ret.second = i;
       }
     } else {
-      if (nums[i] > nums[res[1]]) { // found i,j,k
-        res[2] = i;
-        return res;
-      } else if (nums[i] > nums[res[0]]) {
-        res[1] = i;
-      } else if (nums[i] > nums[min_idx]) {
-        res[0] = min_idx;
-        res[1] = i;
-      } else if (nums[i] < nums[min_idx]) {
+      if (a[i] > a[ret.second]) { // found i,j,k
+        ret.third = i;
+        return ret;
+      } else if (a[i] > a[ret.first]) {
+        ret.second = i;
+      } else if (a[i] > a[min_idx]) {
+        ret.first = min_idx;
+        ret.second = i;
+      } else if (a[i] < a[min_idx]) {
         min_idx = i;
       }
     }
   }
 
-  return {};
+  return {-1,-1,-1};
 }
 
 int main(int argc, char** argv) {
-  vector<int32_t> nums = {2,3,1,4};
-  vector<int32_t> res = threeIndices(nums);
-  if (res.size())
-    cout << "No valid indices!" << endl;
-
-  for (auto i : res)
-    cout << i << " ";
-  cout << endl;
+  int32_t a[] = {2,3,1,4};
+  Indices res = threeIndices(a, 4);
+  cout << res.first << " " << res.second << " " << res.third << endl;
 
   return 0;
 }
