8bd33bbfadd37d3aad6877906daf7652f7347398
pengxu7 <pengxu7@gmail.com>
2015-09-08 16:02:05 -0400

    Update itoa.h

--- itoa.h
+++ itoa.h
@@ -1,33 +1,34 @@
 #ifndef _ITOA_H_
 #define _ITOA_H_
 
-char* itoa(int32_t value, char* result, const uint32_t base = 10) {
+char* itoa(int32_t value, char* buffer, const uint32_t base = 10) {
   if (base < 2 || base > 36) {
-    *result = '\0';
-    return result;
+    *buffer = '\0';
+    return buffer;
   }
 
-  char* ptr = result, *ptr1 = result, tmp_char;
-  int32_t tmp_value;
+  char* ptr = buffer;
+  char* cp = ptr;
+  int32_t tmp_v;
 
   do {
-    tmp_value = value;
+    tmp_v = value;
     value /= base;
-    *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz"[35+(tmp_value-value*base)];
+    *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz"[35+(tmp_v-value*base)];
   } while (value);
 
   // apply negative sign
-  if (tmp_value < 0)
+  if (tmp_v < 0)
     *ptr++ = '-';
   *ptr-- = '\0';
 
-  while (ptr1 < ptr) {
-    tmp_char = *ptr;
-    *ptr--   = *ptr1;
-    *ptr1++  = tmp_char;
+  while (cp < ptr) {
+    char tmp = *ptr;
+    *ptr--   = *cp;
+    *cp++  = tmp;
   }
 
-  return result;
+  return buffer;
 }
 
 #endif // _ITOA_H_
