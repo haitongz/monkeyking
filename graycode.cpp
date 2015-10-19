da97c93f02519641a23e6f14a47e938530667459
pengxu7 <pengxu7@gmail.com>
2015-10-19 15:11:30 -0400

    Update graycode.cpp

--- graycode.cpp
+++ graycode.cpp
@@ -1,5 +1,7 @@
 /*
-The gray code is a binary numeral system where two successive values differ in only one bit. Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+The gray code is a binary numeral system where two successive values differ in only one bit.
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code.
+A gray code sequence must begin with 0.
 
 Given n = 2, return [0,1,3,2]. Its gray code sequence is:
 00 - 0
@@ -17,34 +19,34 @@ using namespace std;
 
 // Refer to http://en.wikipedia.org/wiki/Gray_code
 vector<int32_t> grayCode(const uint32_t n) {
-  vector<int32_t> res;
+  vector<int32_t> ret;
   if (!n) {
-    res.push_back(0);
-    return res;
+    ret.push_back(0);
+    return ret;
   }
 
   vector<int32_t> tmp = grayCode(n-1);
-  res = tmp;
+  ret = tmp;
   for (int32_t i = tmp.size()-1; i >= 0; --i) {
-    res.push_back(tmp[i] | (1 << (n-1)));
+    ret.push_back(tmp[i] | (1 << (n-1)));
   }
 
-  return res;
+  return ret;
 }
 
 vector<int32_t> grayCode2(const uint32_t n) {
-  vector<int32_t> res;
-  res.push_back(0);
+  vector<int32_t> ret;
+  ret.push_back(0);
 
   for (uint32_t i = 0; i < n; ++i) {
     uint32_t highestBit = 1<<i;
-    const uint32_t len = res.size();
+    const uint32_t len = ret.size();
     for (int32_t j = len-1; j >= 0; --j) {
-      res.push_back(highestBit+res[j]);
+      ret.push_back(highestBit+ret[j]);
     }
   }
 
-  return res;
+  return ret;
 }
 
 int main(int argc, char** argv) {
