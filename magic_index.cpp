b33a92b7ebbe6803ac37f9bb06b0d8583acb4ee5
pengxu7 <pengxu7@gmail.com>
2015-10-19 15:46:45 -0400

    Update magic_index.cpp

--- magic_index.cpp
+++ magic_index.cpp
@@ -1,5 +1,6 @@
 /**
-A magic index in an array A[0...n-1] is defined to be an index such that A[i] = i. Given a sorted array, write a method to find a magic index, if one exists, in array A.
+A magic index in an array A[0...n-1] is defined to be an index such that A[i] = i.
+Given a sorted array, write a method to find a magic index, if one exists, in array A.
 
 FOLLOW UP: What if the values are not distinct?
  */
@@ -27,7 +28,8 @@ int32_t magicIndex(const vector<uint32_t>& A) {
     if (t_idx == mid)
       return mid;
 
-    const uint32_t new_end = min(mid-1, t_idx); // optimization: array is sorted so when array[mid] < mid-1, magic index can only appear before index array[mid]
+    const uint32_t new_end = min(mid-1, t_idx); // optimization: array is sorted so when array[mid] < mid-1,
+                                                // magic index can only appear before index array[mid]
     int32_t test_left = solve(start, new_end);
     if (test_left != -1)
       return test_left;
