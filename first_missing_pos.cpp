3cccc94099fdfc3dbabdd9ce63f370fab78fa084
pengxu7 <pengxu7@gmail.com>
2015-10-19 15:05:17 -0400

    Update first_missing_pos.cpp

--- first_missing_pos.cpp
+++ first_missing_pos.cpp
@@ -8,7 +8,12 @@ Given an unsorted integer array, find the first missing positive integer.
 Given [1,2,0] return 3, and [3,4,-1,1] return 2.
 Your algorithm should run in O(n) time and uses constant space.
 
-if nums[i] is positive, say we have nums[i] = x, we know it should be in slot nums[x-1]. We can swap nums[x-1] with nums[i] so as to place x into the right place; if nums[i] is non-positive, 0, or nums[i] > nums.size(), ignore it; if nums[i] == nums[x-1], we do not swap, two cases: 1. Maybe nums[i] = i+1, nums[i] is correct; 2. though nums[i] is not correct like [1,1] index 1, but nums[0] == nums[1], swap nums[1] with nums[0] will only give rise to infinite loop.
+if nums[i] is positive, say we have nums[i] = x, we know it should be in slot nums[x-1].
+We can swap nums[x-1] with nums[i] so as to place x into the right place; if nums[i] is non-positive, 0,
+or nums[i] > nums.size(), ignore it; if nums[i] == nums[x-1], we do not swap, two cases:
+1. Maybe nums[i] = i+1, nums[i] is correct;
+2. though nums[i] is not correct like [1,1] index 1, but nums[0] == nums[1],
+   swap nums[1] with nums[0] will only give rise to infinite loop.
  */
 uint32_t firstMissingPos(vector<int32_t>& A) {
   const uint32_t n = A.size();
@@ -33,13 +38,15 @@ uint32_t firstMissingPos(vector<int32_t>& A) {
 }
 
 /*
-Given an array of integers, find the first repeating element in it. We need to find the element that occurs more than once and whose index of first occurrence is smallest.
+Given an array of integers, find the first repeating element in it. We need to find the element that occurs
+more than once and whose index of first occurrence is smallest.
  */
 int32_t firstRepeating(const vector<int32_t>& a) {
 }
 
 /*
-Given a sorted array of n integers where each integer is in the range from 0 to m-1 and m > n. Find the smallest number that is missing from the array.
+Given a sorted array of n integers where each integer is in the range from 0 to m-1 and m > n.
+Find the smallest number that is missing from the array.
  */
 int32_t firstMissing(const uint32_t array[], const uint32_t n) {
   if (!n)
