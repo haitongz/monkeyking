d0b00a7b6ca4b75f52dec2ae654553ac3c6fa216
pengxu7 <pengxu7@gmail.com>
2015-10-19 14:00:19 -0400

    Update digits_dp.cpp

--- digits_dp.cpp
+++ digits_dp.cpp
@@ -120,7 +120,8 @@ uint32_t digitSumCnt2(const uint32_t n) {
 }
 
 /*
-A number is non-decreasing if every digit (except the first one) is greater than or equal to previous digit. For example, 223, 4455567, 899, are non-decreasing numbers.
+A number is non-decreasing if every digit (except the first one) is greater than or equal to previous digit.
+For example, 223, 4455567, 899, are non-decreasing numbers.
 
 So, given the number of digits n, you are required to find the count of total non-decreasing numbers with n digits.
  */
