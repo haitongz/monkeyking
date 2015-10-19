be35c5ac013c8cf1497f731948ec9be057bd83ca
pengxu7 <pengxu7@gmail.com>
2015-10-19 13:36:17 -0400

    Update concatenation.cpp

--- concatenation.cpp
+++ concatenation.cpp
@@ -1,6 +1,8 @@
 /*
-You are given a string, S, and a list of words, L, that are all of the same length. Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
-For example, given:
+You are given a string, S, and a list of words, L, that are all of the same length. Find all starting indices of
+substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
+
+Given:
 S: "barfoothefoobarman"
 L: ["foo", "bar"]
 You should return the indices: [0,9].
