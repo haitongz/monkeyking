2daebe9922bed419e6a2d76b29bfeeec11ef2ece
pengxu7 <pengxu7@gmail.com>
2015-10-20 10:49:08 -0400

    Update peak_elem.cpp

--- peak_elem.cpp
+++ peak_elem.cpp
@@ -3,10 +3,15 @@ A peak element is an element that is greater than its neighbors.
 
 Given an input array where num[i] != num[i+1], find a peak element and return its index.
 
-The array may contain multiple peaks, in that case return the index to any one of the peaks is fine. You may imagine that num[-1] = num[n] = INT_MIN. For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
- */
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+You may imagine that num[-1] = num[n] = INT_MIN.
 
-//Binary search. If mid is the peak, return it. If mid is not the peak, it must be less than or equal to one of its neighbors. Suppose it is less than or equal to its left, then the left half must contains a peak. Why? If the left half is not monotonically decreasing, there must contains a peak. If the left half is monotonically decreasing, the peak is the first element.
+Given array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+ */
+// Binary search. If mid is the peak, return it. If mid is not the peak, it must be less than or equal to one of its neighbors.
+// Suppose it is less than or equal to its left, then the left half must contains a peak. Why?
+// If the left half is not monotonically decreasing, there must contains a peak.
+// If the left half is monotonically decreasing, the peak is the first element.
 #include <iostream>
 #include <functional>
 
@@ -59,7 +64,7 @@ int32_t peakElement3(const vector<int32_t>& nums) {
   if (!n)
     throw exception();
 
-  function<int32_t(const uint32_t,const uint32_t)> dfs =
+  function<int32_t(const uint32_t,const uint32_t)> solve = // DFS
     [&](const uint32_t start, const uint32_t end) {
     if (start == end)
       return start;
