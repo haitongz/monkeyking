98d5f5d5d77e47d9f5d4fbadf780ea6c962d7056
pengxu7 <pengxu7@gmail.com>
2015-10-20 11:39:22 -0400

    Update word_break.cpp

--- word_break.cpp
+++ word_break.cpp
@@ -1,7 +1,8 @@
 /*
-Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+Given a string s and a dictionary of words dict, determine if s can be segmented into
+a space-separated sequence of one or more dictionary words.
 
-For example, given
+Given
 s = "leetcode",
 dict = ["leet", "code"].
 
