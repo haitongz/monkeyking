25fc41e9e4e666f8e1a6207215f1cb4d79bfbc5d
pengxu7 <pengxu7@gmail.com>
2015-10-20 09:59:24 -0400

    Update multiply_matrices.cpp

--- multiply_matrices.cpp
+++ multiply_matrices.cpp
@@ -3,12 +3,15 @@ Given a sequence of matrices, find the most efficient way to multiply these matr
 to perform the multiplications, but merely to decide in which order to perform the multiplications.
 
 We have many options to multiply a chain of matrices because matrix multiplication is associative. In other words, no matter
-how we parenthesize the product, the result will be the same. For example, if we had four matrices A, B, C, and D, we would have:
+how we parenthesize the product, the result will be the same.
+
+Given four matrices A, B, C, and D, we would have:
 
     (ABC)D = (AB)(CD) = A(BCD) = ....
     
 However, the order in which we parenthesize the product affects the number of simple arithmetic operations needed to compute
-the product, or the efficiency. For example, suppose A is a 10 × 30 matrix, B is a 30 × 5 matrix, and C is a 5 × 60 matrix. Then,
+the product, or the efficiency. For example, suppose A is a 10 × 30 matrix, B is a 30 × 5 matrix, and C is a 5 × 60 matrix.
+Then,
 
     (AB)C = (10×30×5) + (10×5×60) = 1500 + 3000 = 4500 operations
     A(BC) = (30×5×60) + (10×30×60) = 9000 + 18000 = 27000 operations.
@@ -32,7 +35,8 @@ int32_t matrixChainOrder(const int32_t a[], const uint32_t n) {
   // One extra row and one extra column, 0th row and 0th column of dp[][] are not used...
   vector<vector<int32_t>> dp(n, vector<int32_t>(n, 0));
 
-  // dp[i,j] = Minimum number of scalar multiplications needed to compute the matrix A[i]A[i+1]...A[j] = A[i..j] where dimention of A[i] is p[i-1] x p[i]
+  // dp[i,j] = Minimum number of scalar multiplications needed to compute the matrix A[i]A[i+1]...A[j] = A[i..j]
+  // where dimention of A[i] is p[i-1] x p[i]
 
   // L is chain length.
   for (uint32_t L = 2; L < n; ++L) {
