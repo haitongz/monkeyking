c6a699f999c079450caf3b03330e8b2be3f117c1
pengxu7 <pengxu7@gmail.com>
2015-10-20 11:06:28 -0400

    Update segregate.cpp

--- segregate.cpp
+++ segregate.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 /*
-Given an array A[], write a function that segregates even and odd numbers. The functions should put all even numbers first, and then odd numbers.
+Given an array A[], write a function that segregates even and odd numbers.
+The functions should put all even numbers first, and then odd numbers.
 
-Example
 Input = {12, 34, 45, 9, 8, 90, 3}
 Output = {12, 34, 8, 90, 45, 9, 3}
  */
@@ -30,7 +30,9 @@ void segregate(vector<int32_t>& a) {
 }
 
 /*
-Given an array of positive integers. All numbers occur even number of times except one number which occurs odd number of times. Find the number in O(n) time & constant space.
+Given an array of positive integers.
+All numbers occur even number of times except one number which occurs odd number of times.
+Find the number in O(n) time & constant space.
  */
 uint32_t oddOccurNum(const vector<uint32_t>& a) {
   const uint32_t n = a.size();
@@ -43,16 +45,27 @@ uint32_t oddOccurNum(const vector<uint32_t>& a) {
 }
 
 /*
-Given an array containing only 0s and 1s, find the largest subarray which contain equal no of 0s and 1s. Expected time complexity is O(n).
+Given an array containing only 0s and 1s, find the largest subarray which contain equal no of 0s and 1s.
+Expected time complexity is O(n).
  */
 /*
 1) Consider all 0 values as -1. The problem now reduces to find out the maximum length subarray with sum = 0.
-2) Create a temporary array sumleft[] of size n. Store the sum of all elements from a[0] to a[i] in sumleft[i]. This can be done in O(n) time.
-3) There are two cases, the output subarray may start from 0th index or may start from some other index. We will return the max of the values obtained by two cases.
+2) Create a temporary array sumleft[] of size n. Store the sum of all elements from a[0] to a[i] in sumleft[i].
+   This can be done in O(n) time.
+3) There are two cases, the output subarray may start from 0th index or may start from some other index.
+   We will return the max of the values obtained by two cases.
 4) To find the maximum length subarray starting from 0th index, scan the sumleft[] and find the maximum i where sumleft[i] = 0.
-5) Now, we need to find the subarray where subarray sum is 0 and start index is not 0. This problem is equivalent to finding two indexes i & j in sumleft[] such that sumleft[i] = sumleft[j] and j-i is maximum. To solve this, we can create a hash table with size = max-min+1 where min is the minimum value in the sumleft[] and max is the maximum value in the sumleft[]. The idea is to hash the leftmost occurrences of all different values in sumleft[]. The size of hash is chosen as max-min+1 because there can be these many different possible values in sumleft[]. Initialize all values in hash as -1
-6) To fill and use hash[], traverse sumleft[] from 0 to n-1. If a value is not present in hash[], then store its index in hash. If the value is present, then calculate the difference of current index of sumleft[] and previously stored value in hash[]. If this difference is more than maxsize, then update the maxsize.
-7) To handle corner cases (all 1s and all 0s), we initialize maxsize as -1. If the maxsize remains -1, then print there is no such subarray.
+5) Now, we need to find the subarray where subarray sum is 0 and start index is not 0.
+   This problem is equivalent to finding two indexes i & j in sumleft[] such that sumleft[i] = sumleft[j] and j-i is maximum.
+   To solve this, we can create a hash table with size = max-min+1 where min is the minimum value in the sumleft[] and
+   max is the maximum value in the sumleft[]. The idea is to hash the leftmost occurrences of all different values
+   in sumleft[]. The size of hash is chosen as max-min+1 because there can be these many different possible values
+   in sumleft[]. Initialize all values in hash as -1
+6) To fill and use hash[], traverse sumleft[] from 0 to n-1. If a value is not present in hash[],
+   then store its index in hash. If the value is present, then calculate the difference of current index of
+   sumleft[] and previously stored value in hash[]. If this difference is more than maxsize, then update the maxsize.
+7) To handle corner cases (all 1s and all 0s), we initialize maxsize as -1. If the maxsize remains -1,
+  then print there is no such subarray.
  */
 pair<uint32_t,uint32_t> largestSub(const uint8_t a[], const uint32_t n) {
   if (!n)
@@ -77,7 +90,9 @@ pair<uint32_t,uint32_t> largestSub(const uint8_t a[], const uint32_t n) {
   // Now calculate the max value of j-i such that sumleft[i] = sumleft[j].
   // The idea is to create a hash table to store indexes of all visited values.
   // If you see a value again, that it is a case of sumleft[i] = sumleft[j].
-  // Check if this j-i is more than maxsize. The optimum size of hash will be max-min+1 as these many different values of sumleft[i] are possible. Since we use optimum size, we need to shift all values in sumleft[] by min before using them as an index in hash[].
+  // Check if this j-i is more than maxsize. The optimum size of hash will be max-min+1
+  // as these many different values of sumleft[i] are possible. Since we use optimum size,
+  // we need to shift all values in sumleft[] by min before using them as an index in hash[].
   int32_t hash[max_sum-min_sum+1];
   for (auto& i : hash)
     i = -1;
