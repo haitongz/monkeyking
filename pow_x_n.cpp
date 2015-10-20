463bd4b38bc0d6b654f7e1cc3147b4651eb15a0c
pengxu7 <pengxu7@gmail.com>
2015-10-20 10:56:47 -0400

    Update pow_x_n.cpp

--- pow_x_n.cpp
+++ pow_x_n.cpp
@@ -26,7 +26,10 @@ double pow(double x, int32_t n) {
 }
 
 /*
-Consider the binary representation of n. For example, if it is "10001011", then x^n = x^(1+2+8+128) = x^1 * x^2 * x^8 * x^128. Thus, we don't want to loop n times to calculate x^n. To speed up, we loop through each bit, if the i-th bit is 1, then we add x^(1 << i) to the result. Since (1 << i) is a power of 2, x^(1<<(i+1)) = square(x^(1<<i)). The loop executes for a maximum of log(n) times.
+Consider the binary representation of n. For example, if it is "10001011",
+then x^n = x^(1+2+8+128) = x^1 * x^2 * x^8 * x^128. Thus, we don't want to loop n times to calculate x^n.
+To speed up, we loop through each bit, if the i-th bit is 1, then we add x^(1 << i) to the result.
+Since (1 << i) is a power of 2, x^(1<<(i+1)) = square(x^(1<<i)). The loop executes for a maximum of log(n) times.
  */
 double pow_iter(double x, const int32_t n) {
   uint32_t m = abs((double)n); // int might overflow
