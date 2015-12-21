d652c9970a2c375f36afc2e8bc2189cede4a1208
pengxu7 <pengxu7@gmail.com>
2015-12-21 10:36:39 -0500

    Create test_countif.cpp

---
+++ test_countif.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+  std::vector<std::string> store;
+
+  store.push_back("Bar");
+  store.push_back("Baz");
+  store.push_back("Foo");
+  store.push_back("Bee");
+
+  std::cout << std::count_if(store.begin(), store.end(),
+                             std::bind2nd(std::greater<std::string>(), "Baz"))
+            << std::endl;
+
+  return 0;
+}
