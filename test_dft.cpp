b0e221a44268128fa8c1392a8c26cdc491e39899
pengxu7 <pengxu7@gmail.com>
2015-12-21 10:48:02 -0500

    Update test_dft.cpp

--- test_dft.cpp
+++ test_dft.cpp
@@ -15,9 +15,23 @@ public:
   }
 };
 
+class B1 {
+};
+
+class D1 {
+public:
+  int d1;
+};
+
+class D2 : public B1, D1 {
+};
+
 int main(int argc, char** argv) {
   std::auto_ptr<B> b(new D);
   b->f();
   std::auto_ptr<D> d(new D);
   d->f();
+  
+  D2 d2;
+  std::cout << d2.d1 << std::endl;
 }
