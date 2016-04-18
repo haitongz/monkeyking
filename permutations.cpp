b2eac6b1a134d3964a0d8d083c67ad3ce59c49d3
pengxu7 <pengxu7@gmail.com>
2016-04-18 16:12:19 -0400

    Update permutations.cpp

--- permutations.cpp
+++ permutations.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 #include <functional>
 
@@ -11,21 +12,21 @@ A string of length n has n! permutation.
 Below are the permutations of string ABC.
 ABC, ACB, BAC, BCA, CAB, CBA
  */
-set<string> allPermus(string s) {
-  const uint32_t len = s.length();
+set<string> stringPermus_recur(string s) {
+  const uint len = s.length();
   if (!len)
     return {};
 
   set<string> ret;
 
-  function<void(const uint32_t,const uint32_t)> solve =
-    [&](const uint32_t start, const uint32_t end) {
+  function<void(const uint,const uint)> solve =
+    [&](const uint start, const uint end) {
     if (start == end) {
       ret.insert(s);
       return;
     }
 
-    for (uint32_t i = start; i < end+1; ++i) {
+    for (uint i = start; i < end+1; ++i) { // backtracking
       swap(s[start], s[i]);
       solve(start+1, end);
       swap(s[start], s[i]);
@@ -48,24 +49,24 @@ By listing and labeling all of the permutations in order, we get the following s
 Given n and k, return the kth permutation sequence.
 Note: Given n will be between 1 and 9 inclusive.
  */
-string kthPermu(const uint8_t n, uint32_t k) {
-  function<uint32_t(const uint8_t)> factorial =
-    [](const uint8_t n) {
-    uint32_t ret = 1;
+string kthPermu(const uint n, uint k) {
+  function<uint(const uint)> factorial =
+    [](const uint n) {
+    uint ret = 1;
 
-    for (uint8_t i = 2; i < n+1; ++i)
+    for (uint i = 2; i < n+1; ++i)
       ret *= i;
 
     return ret;
   };
 
-  uint32_t total = factorial(n);
+  uint total = factorial(n);
   string candidate = string("123456789").substr(0, n);
   string ret(n, ' ');
 
-  for (uint8_t i = 0; i < n; ++i) {
+  for (uint i = 0; i < n; ++i) {
     total /= (n-i);
-    const uint8_t idx = (k-1)/total;
+    const uint idx = (k-1)/total;
     ret[i] = candidate[idx];
     candidate.erase(idx, 1);
     k -= idx*total;
@@ -81,28 +82,28 @@ For example,
 [1,2,3] have the following permutations:
 [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2] and [3,2,1].
  */
-vector<vector<int32_t>> allPermus_recur(vector<int32_t>& nums) {
-  const uint32_t n = nums.size();
+vector<vector<int>> numberPermus_recur(vector<int>& nums) {
+  const uint n = nums.size();
   if (n <= 1)
     return {nums};
 
-  vector<vector<int32_t>> ret;
+  vector<vector<int>> ret;
 
-  function<void(const uint32_t)> backtrack =
-    [&](const uint32_t idx) {
+  function<void(const uint)> solve =
+    [&](const uint idx) {
     if (idx == n) {
       ret.push_back(nums);
       return;
     }
 
-    for (uint32_t i = idx; i < n; ++i) {
+    for (uint i = idx; i < n; ++i) { // backtracking
       swap(nums[i], nums[idx]);
-      backtrack(idx+1);
+      solve(idx+1);
       swap(nums[i], nums[idx]);
     }
   };
 
-  backtrack(0);
+  solve(0);
   return ret;
 }
 
@@ -112,21 +113,21 @@ For example,
 [1,1,2] have the following unique permutations:
 [1,1,2], [1,2,1], [2,1,1].
  */
-vector<vector<int32_t>> uniquePermus(vector<int32_t>& nums) {
-  const uint32_t n = nums.size();
+vector<vector<int>> uniquePermus(vector<int>& nums) {
+  const uint n = nums.size();
   if (n <= 1)
     return {nums};
 
   sort(nums.begin(), nums.end());
 
-  function<bool(void)> nextPermu = [&](void) -> bool {
-    uint32_t first = 0, last = n;
-    uint32_t pre_last = last-1;
+  function<bool()> nextPermu = [&](){
+    uint first = 0, last = n;
+    uint pre_last = last-1;
 
     while (first < pre_last) {
-      uint32_t t = pre_last;
+      uint t = pre_last;
       if (nums[--pre_last] < nums[t]) {
-        uint32_t l = last;
+        uint l = last;
         while (nums[--l] <= nums[pre_last]);
 
         swap(nums[l], nums[pre_last]);
@@ -141,7 +142,7 @@ vector<vector<int32_t>> uniquePermus(vector<int32_t>& nums) {
     }
   };
 
-  vector<vector<int32_t>> ret;
+  vector<vector<int>> ret;
 
   do {
     ret.push_back(nums);
@@ -151,13 +152,21 @@ vector<vector<int32_t>> uniquePermus(vector<int32_t>& nums) {
 }
 
 int main(int argc, char** argv) {
-  uint32_t n = 3;
-  uint32_t k = 5;
+  string s = "ABC";
+  cout << "All permutations of " << s << " are: ";
+  set<string> permus = stringPermus_recur(s);
+  for (const auto& i : permus) {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  uint n = 3;
+  uint k = 5;
   cout << k << "th permutation of [1,..." << n << "]:" << endl;
   cout << kthPermu(3, 5) << endl;
 
-  vector<int32_t> nums = {1,2,3};
-  vector<vector<int32_t>> res = allPermus_recur(nums);
+  vector<int> nums = {1,2,3};
+  vector<vector<int>> res = numberPermus_recur(nums);
 
   cout << "All permutations of {1,2,3}:" << endl;
   for (auto& i : res) {
