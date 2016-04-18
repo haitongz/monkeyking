f908fb939bbaf60f21f38acc5811a749a9086db3
pengxu7 <pengxu7@gmail.com>
2016-04-18 16:10:54 -0400

    Update phone_chess.cpp

--- phone_chess.cpp
+++ phone_chess.cpp
@@ -8,21 +8,21 @@
 
 using namespace std;
 
-typedef pair<int32_t,int32_t> Coordinates;
+typedef pair<int,int> Coordinates;
 typedef function<vector<Coordinates>(const Coordinates)> MovesFinder;
 
-vector<vector<int8_t>> grid; // store the input grid
-map<const uint8_t,Coordinates> coors; // for looking up coordinates of a valid value, 10 keys at most
+vector<vector<int>> grid; // store the input grid
+map<const uint,Coordinates> coors; // for looking up coordinates of a valid value, 10 keys at most
 
 // this method returns i and j of input value
-Coordinates getCoordinates(const uint8_t value) {
+Coordinates getCoordinates(const uint value) {
     return coors.count(value) ? coors[value] : Coordinates(-1,-1);
 }
 
 // check whether an input coordinates is in board and not -1
 bool isValidCell(const Coordinates& c) {
-    const uint32_t rows = grid.size();
-    const uint32_t cols = grid[0].size();
+    const uint rows = grid.size();
+    const uint cols = grid[0].size();
 
     return c.first >= 0 && c.first < rows &&
            c.second >= 0 && c.second < cols &&
@@ -44,8 +44,8 @@ nextKnightMoves(Coordinates start) {
     // can it choose to stay or not?
     // ret.push_back(start);
 
-    const int32_t i = start.first;
-    const int32_t j = start.second;
+    const int i = start.first;
+    const int j = start.second;
 
     if (isValidCell({i+1,j+2})) {
         ret.push_back({i+1,j+2});
@@ -83,16 +83,16 @@ nextBishopMoves(Coordinates start) {
 
     vector<Coordinates> ret;
 
-    const uint32_t i = start.first;
-    const uint32_t j = start.second;
-    uint32_t k = 0;
-    uint32_t l = 0;
-    const uint32_t rows = grid.size();
-    const uint32_t cols = grid[0].size();
+    const uint i = start.first;
+    const uint j = start.second;
+    uint k = 0;
+    uint l = 0;
+    const uint rows = grid.size();
+    const uint cols = grid[0].size();
 
-    for (uint32_t k = 0; k < rows; ++k) {
-        for (uint32_t l = 0; l < cols; ++l) {
-            int32_t xdiff = k-i, ydiff = l-j;
+    for (uint k = 0; k < rows; ++k) {
+        for (uint l = 0; l < cols; ++l) {
+            int xdiff = k-i, ydiff = l-j;
             if (xdiff &&
                 abs(xdiff) == abs(ydiff) &&
                 isValidCell({k,l})) {
@@ -105,8 +105,8 @@ nextBishopMoves(Coordinates start) {
 }
 
 // get the number of valid phone numbers with different functions
-uint32_t getNumber(const uint32_t N,
-                   const vector<uint8_t>& starts,
+uint getNumber(const uint N,
+                   const vector<uint>& starts,
                    const MovesFinder& func) {
     // If we want to calculate the number of 7-digit valid phone numbers that
     // Knight/Bishop can generate starting from point, for example 1, then
@@ -114,24 +114,24 @@ uint32_t getNumber(const uint32_t N,
     // 8 and number of 6-digit valid phone numbers starting from 6.
     // Complexity: O(N) as we fill a matrix of dimension (N+1)*10 one cell a time.
 
-    uint32_t mtable[N+1][10]; // one extra row for digit 0
+    uint mtable[N+1][10]; // one extra row for digit 0
 
-    for (uint32_t i = 0; i < N+1; ++i) {
-        for (uint8_t j = 0; j < 10; ++j) {
+    for (uint i = 0; i < N+1; ++i) {
+        for (uint j = 0; j < 10; ++j) {
             mtable[i][j] = 0; // initial cell values are 0
         }
     }
-    for (uint8_t i = 0; i < 10; ++i) {
+    for (uint i = 0; i < 10; ++i) {
         mtable[1][i] = 1; // there's only 1-digit number at any point
     }
 
-    function<uint32_t(const uint32_t,const Coordinates)> solve =
-    [&](const uint32_t digits, const Coordinates& c) -> uint32_t {
+    function<uint(const uint,const Coordinates)> solve =
+    [&](const uint digits, const Coordinates& c) -> uint {
         if (!digits) {
             return 0;
         }
 
-        uint8_t value = grid[c.first][c.second];
+        uint value = grid[c.first][c.second];
         if (!mtable[digits][value]) {
             vector<Coordinates> moves = func(c);
             for (auto n : moves) {
@@ -142,7 +142,7 @@ uint32_t getNumber(const uint32_t N,
         return mtable[digits][value];
     };
 
-    uint32_t ret = 0;
+    uint ret = 0;
     for (const auto s : starts) {
         Coordinates c = getCoordinates(s);
         ret += solve(N, c);
@@ -176,7 +176,7 @@ int main(int argc, char** argv) {
         string piece;
         getline(cin, piece);
         if (strcasecmp(piece.c_str(), "knight") && strcasecmp(piece.c_str(), "bishop")) {
-            cerr << "Unsupported piece! Please choose Bishop or Knight." << endl;
+            cerr << "Wrong piece!" << endl;
             continue;
         }
 
@@ -187,7 +187,7 @@ int main(int argc, char** argv) {
             continue;
         }
 
-        const uint32_t digits = atoi(numStr.c_str());
+        const uint digits = atoi(numStr.c_str());
         if (!digits) {
             cerr << "Wrong number of digits!" << endl;
             continue;
@@ -197,7 +197,7 @@ int main(int argc, char** argv) {
         getline(cin, numStr);
         istringstream iss(numStr);
         vector<string> arr{istream_iterator<string>{iss}, istream_iterator<string>{}};
-        vector<uint8_t> starts;
+        vector<uint> starts;
         bool restart = false;
 
         for (const auto& i : arr) {
@@ -220,7 +220,7 @@ int main(int argc, char** argv) {
             continue;
         }
 
-        const uint32_t rows = atoi(numStr.c_str());
+        const uint rows = atoi(numStr.c_str());
 
         cout << "columns: ";
         getline(cin, numStr);
@@ -228,10 +228,10 @@ int main(int argc, char** argv) {
             continue;
         }
 
-        const uint32_t cols = atoi(numStr.c_str());
+        const uint cols = atoi(numStr.c_str());
 
-        for (uint32_t i = 0; i < rows; ++i) {
-            grid.push_back(vector<int8_t>());
+        for (uint i = 0; i < rows; ++i) {
+            grid.push_back(vector<int>());
             cout << "space-separated characters: ";
             getline(cin, numStr);
             istringstream iss2(numStr);
@@ -242,7 +242,7 @@ int main(int argc, char** argv) {
                 break;
             }
 
-            for (uint32_t j = 0; j < cols; ++j) {
+            for (uint j = 0; j < cols; ++j) {
                 if (digits[j].length() != 1) {
                     cerr << "Wrong row content!" << endl;
                     restart = true;
@@ -250,7 +250,7 @@ int main(int argc, char** argv) {
                 }
 
                 if (isdigit(digits[j][0])) {
-                    uint8_t value = atoi(digits[j].c_str());
+                    uint value = atoi(digits[j].c_str());
                     grid.back().push_back(value);
                     coors[value] = {i,j};
                 } else {
