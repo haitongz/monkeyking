0663002c62b29399e436cf2f17c5e1f72d49369f
pengxu7 <pengxu7@gmail.com>
2016-04-18 16:22:02 -0400

    Update intervals.cpp

--- intervals.cpp
+++ intervals.cpp
@@ -14,45 +14,20 @@ Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10
 This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
  */
 struct Interval {
-  uint32_t start, end;
-
-  Interval(const uint32_t s, const uint32_t e)
-      : start(s), end(e) {
+  Interval(const uint s, const uint e)
+    : start(s), end(e) {
   }
+
+  uint start, end;
 };
-/*
-vector<Interval> insert(vector<Interval>& itvls, Interval nv) {
-  vector<Interval> res;
-  uint32_t i = 0;
 
-  for (; i < itvls.size() && itvls[i].end < nv.start; ++i) {
-    res.push_back(itvls[i]);
-  }
-
-  if (i == itvls.size()) {
-    res.push_back(nv);
-    return res;
-  }
-
-  nv.start = min(nv.start, itvls[i].start);
-  for (; i < itvls.size() && itvls[i].start <= nv.end; ++i) {
-    nv.end = max(nv.end, itvls[i].end);
-  }
-  res.push_back(nv);
-
-  for (; i < itvls.size(); ++i) {
-    res.push_back(itvls[i]);
-  }
-
-  return res;
-}
-*/
-vector<Interval> insert(vector<Interval>& itvls, Interval nv) {
-  const uint32_t n = itvls.size();
+vector<Interval>
+insertMerge(vector<Interval>& itvls, Interval nv) {
+  const uint n = itvls.size();
   if (!n)
     return {nv};
 
-  uint32_t idx = 0;
+  uint idx = 0;
   vector<Interval> ret;
 
   while (idx < n && itvls[idx].end < nv.start) {
@@ -70,54 +45,7 @@ vector<Interval> insert(vector<Interval>& itvls, Interval nv) {
 
   return ret;
 }
-/*
-vector<Interval> insert_2(vector<Interval>& itvls, Interval ni) {
-  const uint32_t n = itvls.size();
-  if (!n)
-    return {ni};
 
-  function<int32_t(const bool)> find = [&](const bool find_max) -> int32_t {
-    if (!find_max && ni.start <= itvls.front().end)
-      return -1;
-    else if (find_max && ni.end >= itvls.back().start)
-      return n;
-
-    uint32_t left = 0, right = n-1;
-    while (left <= right) {
-      const uint32_t mid = left + (right-left)/2;
-      if (itvls[mid].end < ni.start) {
-        left = mid+1;
-      } else if(itvls[mid].start > ni.end) {
-        right = mid-1;
-      } else {
-        if (find_max)
-          left = mid+1;
-        else
-          right = mid-1;
-      }
-    }
-
-    return find_max ? left : right;
-  };
-
-  int32_t max_id = find(true), min_id = find(false);
-  vector<Interval> res;
-
-  copy(itvls.begin(), (itvls.end()-min_id), back_inserter(res));
-
-  uint32_t new_start = ni.start, new_end = ni.end;
-  int overlap_l = min_id+1, overlap_r = max_id-1;
-  if(overlap_l <= overlap_r && overlap_r >= 0) {
-    new_start = min(new_start, itvls[overlap_l].start);
-    new_end = max(new_end, itvls[overlap_r].end);
-  }
-
-  res.emplace_back(new_start, new_end);
-  copy((itvls.begin()+max_id+1), itvls.end(), back_inserter(res));
-
-  return res;
-}
-*/
 /*
 Given a array of pairs where each pair contains the start and end time of a meeting (as in integer),
 determine if a single person can attend all the meetings
@@ -133,41 +61,43 @@ Input array { pair(1, 4), pair(2,3), pair(3,4), pair(4,5) }
 Output: 2
  */
 bool canAttendAll(vector<Interval>& mtgs) {
-  const uint32_t n = mtgs.size();
+  const uint n = mtgs.size();
   if (!n)
     return false;
 
   sort(mtgs.begin(), mtgs.end(), [&](const Interval& a, const Interval& b) {return a.start < b.start;});
 
-  for (uint32_t i = 1; i < n; ++i) {
-    if (mtgs[i].start < mtgs[i-1].end)
+  for (uint i = 1; i < n; ++i) {
+    if (mtgs[i].start < mtgs[i-1].end) {
       return false;
+    }
   }
 
   return true;
 }
 
-uint32_t minRooms(vector<Interval>& mtgs) {
-  const uint32_t n = mtgs.size();
+uint minRooms(vector<Interval>& mtgs) {
+  const uint n = mtgs.size();
   if (!n)
     return 0;
 
-  vector<int32_t> times;
+  vector<int> times;
 
-  for (auto m : mtgs) {
+  for (const auto& m : mtgs) {
     times.push_back(m.start);
     times.push_back(-m.end);
   }
 
-  sort(times.begin(), times.end(), [](int32_t a, int32_t b) { return (abs(a) == abs(b)) ? (a < b) : (abs(a) < abs(b)); });
+  sort(times.begin(), times.end(), [](const int a, const int b) {return (abs(a) == abs(b)) ? (a < b) : (abs(a) < abs(b));});
 
-  uint32_t ret = 0, curr = 0;
+  uint ret = 0, curr = 0;
 
-  for (auto t : times) {
-    if (t >= 0)
+  for (const auto t : times) {
+    if (t >= 0) {
       ret = max(ret, ++curr);
-    else
+    } else {
       --curr;
+    }
   }
 
   return ret;
@@ -182,28 +112,27 @@ Given pairs {{5, 24}, {39, 60}, {15, 28}, {27, 40}, {50, 90}}, then the longest 
 and the chain is {{5, 24}, {27, 40}, {50, 90}}
  */
 struct Pair {
-  Pair(const int32_t s, const int32_t e)
+  Pair(const int s, const int e)
       : a(s), b(e) {
   }
 
-  int32_t a;
-  int32_t b;
+  int a, b;
 };
 
 bool operator<(const Pair& p1, const Pair& p2) {
   return p1.b < p2.b;
 }
 
-uint32_t maxPairChainLen_greedy(vector<Pair>& p) {
-  const uint32_t n = p.size();
+uint maxPairChainLen_greedy(vector<Pair>& p) {
+  const uint n = p.size();
   if (n < 2)
     return n;
 
   sort(p.begin(), p.end());
-  uint32_t active_p = 0;
-  uint32_t ret = 1;
+  uint active_p = 0;
+  uint ret = 1;
 
-  for (uint32_t i = 1; i < n; ++i) {
+  for (uint i = 1; i < n; ++i) {
     if (p[i].a > p[active_p].b) {
       ++ret;
       active_p = i;
@@ -213,14 +142,15 @@ uint32_t maxPairChainLen_greedy(vector<Pair>& p) {
   return ret;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   Interval i1(1, 3), i2(6, 9), i3(2, 5);
   vector<Interval> ivs;
   ivs.push_back(i1);
   ivs.push_back(i2);
 
-  vector<Interval> res = insert(ivs, i3);
-  for (uint32_t i = 0; i < res.size(); ++i) {
+  vector<Interval> res = insertMerge(ivs, i3);
+  for (uint i = 0; i < res.size(); ++i) {
     cout << "[" << res[i].start << "," << res[i].end << "],";
   }
   cout << endl;
@@ -235,8 +165,8 @@ int main(int argc, char** argv) {
   ivs.push_back(i3);
   ivs.push_back(i4);
   ivs.push_back(i5);
-  res = insert(ivs, Interval(4, 9));
-  for (uint32_t i = 0; i < res.size(); ++i) {
+  res = insertMerge(ivs, Interval(4, 9));
+  for (uint i = 0; i < res.size(); ++i) {
     cout << "[" << res[i].start << "," << res[i].end << "],";
   }
   cout << endl;
@@ -247,7 +177,7 @@ int main(int argc, char** argv) {
   ivs[1] = Interval(4, 5);
   ivs[2] = Interval(3, 4);
   ivs[3] = Interval(2, 3);
-  cout << "A person " << (canAttendAll(ivs) ? "can " : "cannot ") << "attend all meetings." << endl;
+  cout << "A person " << (canAttendAll(ivs) ? "can" : "cannot") << " attend all meetings." << endl;
 
   ivs[1] = Interval(2, 3);
   ivs[3] = Interval(4, 5);
