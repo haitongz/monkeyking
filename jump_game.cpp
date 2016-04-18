f6544653c58e7fbbbeeb379e61dafe434e1cb2a3
pengxu7 <pengxu7@gmail.com>
2016-04-18 16:08:09 -0400

    Update jump_game.cpp

--- jump_game.cpp
+++ jump_game.cpp
@@ -6,7 +6,11 @@
 
 using namespace std;
 
-static const uint32_t MAX_LMT = numeric_limits<uint32_t>::max();
+static const int MAX_LMT = numeric_limits<int>::has_infinity ?
+                           numeric_limits<int>::infinity() : numeric_limits<int>::max();
+
+static const int MIN_LMT = numeric_limits<int>::has_infinity ?
+                           -1*numeric_limits<int>::infinity() : numeric_limits<int>::min();
 
 /*
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
@@ -16,13 +20,13 @@ For example:
 A = [2,3,1,1,4], return true.
 A = [3,2,1,0,4], return false.
  */
-bool canJump(const vector<uint32_t>& A) {
-  const uint32_t n = A.size();
+bool canJump(const vector<uint>& A) {
+  const uint n = A.size();
   if (!n)
     return false;
 
-  uint32_t reach = 0;
-  for (uint32_t i = 0; i <= reach /*no need to continue if there's a gap*/&& i < n; ++i) {
+  uint reach = 0;
+  for (uint i = 0; i <= reach /*no need to continue if there's a gap*/&& i < n; ++i) {
     reach = max(i+A[i], reach);
   }
 
@@ -39,28 +43,28 @@ Your goal is to reach the last index in the minimum number of jumps.
 Given array A = [2,3,1,1,4]
 The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
  */
-uint32_t minJumps_recur(const vector<uint32_t>& A) {
-  const uint32_t n = A.size();
+uint minJumps_recur(const vector<uint>& A) {
+  const uint n = A.size();
   if (!n)
     return MAX_LMT;
 
-  function<uint32_t(const uint32_t,const uint32_t)> solve = // DFS
-    [&](const uint32_t start, const uint32_t end) -> uint32_t {
+  function<uint(const uint,const uint)> solve = // DFS
+    [&](const uint start, const uint end) -> uint {
     // when source and destination are same
     if (start == end)
       return 0;
 
     // When nothing is reachable from the given source
     if (!A[start])
-      return max_lmt;
+      return MAX_LMT;
 
     // Traverse through all the points reachable from A[l].
     // Recursively get the minimum number of jumps needed to reach arr[h] from these reachable points.
-    uint32_t ret = max_lmt;
+    uint ret = MAX_LMT;
 
-    for (uint32_t i = start+1; i < end+1 && i < start+A[start]+1; ++i) {
-      uint32_t jumps = solve(i, end);
-      if (jumps != max_lmt)
+    for (uint i = start+1; i < end+1 && i < start+A[start]+1; ++i) {
+      uint jumps = solve(i, end);
+      if (jumps != MAX_LMT)
         ret = min(ret, jumps+1);
     }
 
@@ -70,18 +74,18 @@ uint32_t minJumps_recur(const vector<uint32_t>& A) {
   return solve(0, n-1);
 }
 
-uint32_t minJumps_dp(const vector<uint32_t>& A) {
-  const uint32_t n = A.size();
+uint minJumps_dp(const vector<uint>& A) {
+  const uint n = A.size();
   if (!n || !A[0])
-    return max_lmt;
+    return MAX_LMT;
 
-  uint32_t dp[n];
+  uint dp[n];
   dp[0] = 0;
-  for (uint32_t i = 1; i < n; ++i) {
-    dp[i] = max_lmt;
+  for (uint i = 1; i < n; ++i) {
+    dp[i] = MAX_LMT;
 
-    for (uint32_t j = 0; j < i; ++j) {
-      if (i <= j+A[j] && dp[j] != max_lmt) {
+    for (uint j = 0; j < i; ++j) {
+      if (i <= j+A[j] && dp[j] != MAX_LMT) {
         dp[i] = min(dp[i], dp[j]+1);
         break;
       }
@@ -96,24 +100,24 @@ Given a binary array R representing a river: 0 means water and 1 means stone.
 Initial position R[0] is 1, initial speed is 1. You can choose to jump at old speed, or at speed+1 for next move.
 Find minimum number of steps to jump river.
  */
-uint32_t minRiverJumps_recur(const vector<uint8_t>& R) {
+uint minRiverJumps_recur(const vector<uint8_t>& R) {
   if (R.empty())
     return 0;
 
-  map<pair<uint32_t,uint32_t>,int32_t> recs;
+  map<pair<uint,uint>,int> recs;
 
-  function<int32_t(const uint32_t, const uint32_t)> solve = // DFS
-    [&](const uint32_t speed, const uint32_t idx) -> int32_t {
+  function<int(const uint, const uint)> solve = // DFS
+    [&](const uint speed, const uint idx) -> int {
     if (idx >= R.size())
       return 0;
 
     if (recs.count({idx,speed}))
       return recs[{idx,speed}];
 
-    int32_t ret = -1;
+    int ret = -1;
     if (R[idx]) {
-      int32_t res0 = solve(speed, idx+speed);
-      int32_t res1 = solve(speed+1, idx+speed+1);
+      int res0 = solve(speed, idx+speed);
+      int res1 = solve(speed+1, idx+speed+1);
 
       if (res0 >= 0 && res1 >= 0) {
         ret = min(res0, res1) + 1;
@@ -131,15 +135,15 @@ uint32_t minRiverJumps_recur(const vector<uint8_t>& R) {
   return solve(1, 0);
 }
 
-int32_t minRiverJumps_dp(const vector<uint8_t>& R) {
-  const uint32_t n = R.size();
+int minRiverJumps_dp(const vector<uint8_t>& R) {
+  const uint n = R.size();
   if (!n)
     return 0;
 
-  vector<vector<pair<uint32_t,uint32_t>>> dp(n);
+  vector<vector<pair<uint,uint>>> dp(n);
   dp[0].emplace_back(1, 1);
 
-  uint32_t ret = max_lmt;
+  uint ret = MAX_LMT;
 
   for (auto i = 0; i < n; ++i) {
     if (!R[i])
@@ -159,12 +163,12 @@ int32_t minRiverJumps_dp(const vector<uint8_t>& R) {
     }
   }
 
-  return (ret == max_lmt) ? -1 : (int32_t)ret;
+  return (ret == MAX_LMT) ? -1 : (int)ret;
 }
 
 int main(int argc, char** argv) {
-  vector<uint32_t> A = {2, 3, 1, 1, 4};
-  vector<uint32_t> B = {3, 2, 1, 0, 4};
+  vector<uint> A = {2, 3, 1, 1, 4};
+  vector<uint> B = {3, 2, 1, 0, 4};
 
   cout << "Can reach end? " << (canJump(A) ? "true" : "false") << endl;
   cout << "Can reach end? " << (canJump(B) ? "true" : "false") << endl;
