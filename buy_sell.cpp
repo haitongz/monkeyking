e0ddf5be3eba9e24a221e2340e01786c5d20d258
pengxu7 <pengxu7@gmail.com>
2016-05-09 10:12:30 -0400

    Update buy_sell.cpp

--- buy_sell.cpp
+++ buy_sell.cpp
@@ -8,7 +8,7 @@ Say you have an array for which the ith element is the price of a given stock on
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
 design an algorithm to find the maximum profit.
  */
-double maxProfit(const double prices[], const uint32_t n) {
+double maxProfit_1Trans(const double prices[], const uint n) {
   if (n < 2) {
 #ifdef _DEBUG_
     cerr << "Too few days!" << endl;
@@ -17,18 +17,18 @@ double maxProfit(const double prices[], const uint32_t n) {
   }
 
   double lowest_px = prices[0];
-  double res = 0.0;
+  double ret = 0.0;
 
-  for (uint32_t i = 1; i < n; ++i) { // time series when scanning from first to last
+  for (uint i = 1; i < n; ++i) { // time series when scanning from first to last
     if (prices[i] < lowest_px) {
       lowest_px = prices[i]; // no need to check max_profit because you have to buy first sell later
     } else {
-      const double maxpft_sellati = prices[i]-lowest_px;
-      res = max(res, maxpft_sellati);
+      const double maxpft_ati = prices[i]-lowest_px;
+      ret = max(ret, maxpft_ati);
     }
   }
 
-  return res;
+  return ret;
 }
 
 /*
@@ -38,7 +38,7 @@ Design an algorithm to find the maximum profit. You may complete as many transac
 (ie, buy one and sell one share of the stock multiple times).
 However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
  */
-double maxProfit2(const double prices[], const uint32_t n) {
+double maxProfit_anyTrans(const double prices[], const uint n) {
   if (n < 2) {
 #ifdef _DEBUG_
     cerr << "Too few days!" << endl;
@@ -47,7 +47,7 @@ double maxProfit2(const double prices[], const uint32_t n) {
   }
 
   double res = 0.0;
-  for (uint32_t i = 1; i < n; ++i) {
+  for (uint i = 1; i < n; ++i) {
     res += max(prices[i]-prices[i-1], 0.0); // logic: price 1,2,3, profit is same between (3-1) and (2-1+3-2)
   }
 
@@ -64,7 +64,7 @@ You may not engage in multiple transactions at the same time (ie, you must sell 
 // global[i][j] = max(local[i][j], global[i-1][j])
 /*
 double maxProfit3(const vector<double>& prices) {
-  const uint32_t n = prices.size();
+  const uint n = prices.size();
   if (n < 2)
     throw excpetion();
 
@@ -72,13 +72,13 @@ double maxProfit3(const vector<double>& prices) {
   dp[0] = 0.0;
   double lowest_px = numeric_limits<double>::max();
 
-  for (uint32_t i = 0; i < n; ++i) {
+  for (uint i = 0; i < n; ++i) {
     lowest_px = min(prices[i], lowest_px);
     dp[i+1] = max(prices[i]-lowest_px, dp[i]);
   }
 
   double highest_px = numeric_limits<double>::min();
-  for (int32_t i = n-1; i >= 0; --i) {
+  for (int i = n-1; i >= 0; --i) {
     highest_px = max(prices[i], highest_px);
     dp[i] = max(dp[i+1], highest_px-prices[i]+dp[i]);
   }
@@ -86,17 +86,17 @@ double maxProfit3(const vector<double>& prices) {
   return dp[0];
 }
 */
-double maxProfit3(const double prices[], const uint32_t n) {
+double maxProfit_2Trans(const double prices[], const uint n) {
   if (n < 2)
     return 0.0; //throw excpetion();
 
   double local[3];
   double global[3];
 
-  for (uint32_t i = 0; i < n-1; ++i) {
+  for (uint i = 0; i < n-1; ++i) {
     const double diff = prices[i+1]-prices[i];
 
-    for (uint32_t j = 2; j >= 1; --j) {
+    for (uint j = 2; j >= 1; --j) {
       local[j] = max(global[j-1]+max(diff, 0.0), local[j]+diff);
       global[j] = max(local[j], global[j]);
     }
@@ -110,7 +110,7 @@ Say you have an array for which the ith element is the price of a given stock on
 
 Design an algorithm to find the maximum profit. You may complete at most k transactions.
  */
-double maxProfit4(const double prices[], const uint32_t n, uint32_t k) {
+double maxProfit_kTrans(const double prices[], const uint n, uint k) {
   if (n < 2 || k < 1)
     return 0.0;
   if (k > n)
@@ -124,10 +124,10 @@ double maxProfit4(const double prices[], const uint32_t n, uint32_t k) {
   local[0][0] = 0.0;
   global[0][0] = 0.0;
 
-  for (uint32_t i = 1; i < n; ++i) {
+  for (uint i = 1; i < n; ++i) {
     const double diff = prices[i]-prices[i-1];
 
-    for (uint32_t j = 1; j <= k; ++j) {
+    for (uint j = 1; j <= k; ++j) {
       // There are total j transactions made till day i. Two different senarios:
       // 1. jth transaction (buy and sell) takes place on day i;
       // 2. a position was bought earlier but sold on day i. the profit is equal to profit on day i-1
@@ -149,7 +149,7 @@ Output: 87
 Trader earns 87 as sum of 12 and 75
 Buy at price 10, sell at 22, buy at 5 and sell at 80
   */
-double maxProfit5(const double px[], const uint32_t n) {
+double maxProfit_2TransADay(const double px[], const uint n) {
   double profit[n];
   for (auto& i : profit)
     i = 0;
@@ -158,7 +158,7 @@ double maxProfit5(const double px[], const uint32_t n) {
      After this loop, profit[i] contains maximum profit from price[i...n-1]
      using at most one trans. */
   double max_px = px[n-1];
-  for (int32_t i = n-2; i >= 0; --i) {
+  for (int i = n-2; i >= 0; --i) {
     // max_price has maximum of price[i..n-1]
     max_px = max(px[i], max_px);
     // we can get profit[i] by taking maximum of:
@@ -170,7 +170,7 @@ double maxProfit5(const double px[], const uint32_t n) {
   /* Get the maximum profit with two transactions allowed
      After this loop, profit[n-1] contains the result */
   double min_px = px[0];
-  for (uint32_t i = 1; i < n; ++i) {
+  for (uint i = 1; i < n; ++i) {
     // min_px is minimum price in price[0...i]
     min_px = min(px[i], min_px);
     // Maximum profit is maximum of:
@@ -181,6 +181,14 @@ double maxProfit5(const double px[], const uint32_t n) {
 
   return profit[n-1];
 }
+
+/*
+If a trader is allowed to buy a share first and sell'em all in one day (buy->buy->buy->sell).
+Given stock prices throughout day, find out maximum profit that a share trader could have made.
+Input:  px[] = {10, 22, 5, 75, 65, 70}
+Output: 65+53+70+5=193
+Buy at price 10, 22 and 5, then sell at 75. Buy at 65 then sell at 70
+ */
 
 /*
 The advice to "buy low" is half the formula to success in the bovine stock market.
@@ -225,26 +233,26 @@ Sample Input
 Sample Output
 4 2
  */
-const uint16_t MAX_N = 5001;
-uint16_t prices[MAX_N]; // stock prices
+const uint MAX_N = 5001;
+uint prices[MAX_N]; // stock prices
 
-pair<int32_t,int32_t> buylowBuylower(const uint32_t N) {
+pair<int,int> buylowBuylower(const uint N) {
   if (N < 2)
     return {-1,-1};
 
-  uint32_t longest_subseq_cnt[MAX_N]; // how many longest subseqs with index i
-  uint32_t longest_subseq_len[MAX_N]; // longest_subseq_len[i]: how long longest subseqs with index i
+  uint longest_subseq_cnt[MAX_N]; // how many longest subseqs with index i
+  uint longest_subseq_len[MAX_N]; // longest_subseq_len[i]: how long longest subseqs with index i
 
   longest_subseq_cnt[0] = longest_subseq_len[0] = 1;
 
-  for (uint32_t i = 1; i < N; ++i) {
+  for (uint i = 1; i < N; ++i) {
     longest_subseq_len[i] = 1;
 
-    for (uint32_t j = 0; j < i; ++j) {
+    for (uint j = 0; j < i; ++j) {
       if (prices[j] > prices[i] && longest_subseq_len[i] < longest_subseq_len[j]+1)
         longest_subseq_len[i] = longest_subseq_len[j]+1; // calc length of longest subseq with index i
     }
-    for (uint32_t j = 0; j < i; ++j) {
+    for (uint j = 0; j < i; ++j) {
       if (prices[j] > prices[i] && longest_subseq_len[i] == longest_subseq_len[j]+1)
         longest_subseq_cnt[i] += longest_subseq_cnt[j]; // calc how many longest subseqs
     }
@@ -252,19 +260,19 @@ pair<int32_t,int32_t> buylowBuylower(const uint32_t N) {
     if (!longest_subseq_cnt[i])
       longest_subseq_cnt[i] = 1; // incrementing
 
-    for (uint32_t j = 0; j < i; ++j) {
+    for (uint j = 0; j < i; ++j) {
       if (prices[j] == prices[i] && longest_subseq_len[i] == longest_subseq_len[j])
         longest_subseq_cnt[i] -= longest_subseq_cnt[j]; // remove duplicates
     }
   }
 
-  uint32_t len = 0;
-  for (uint32_t i = 0; i < N; ++i) {
+  uint len = 0;
+  for (uint i = 0; i < N; ++i) {
     if (len < longest_subseq_len[i])
       len = longest_subseq_len[i]; // find longest subseqs
   }
-  uint32_t cnt = 0;
-  for (uint32_t i = 0; i < N; ++i) {
+  uint cnt = 0;
+  for (uint i = 0; i < N; ++i) {
     if (len == longest_subseq_len[i])
       cnt += longest_subseq_cnt[i]; // find longest subseqs
   }
@@ -273,26 +281,26 @@ pair<int32_t,int32_t> buylowBuylower(const uint32_t N) {
 }
 
 int main(int argc, char** argv) {
-/*  uint32_t N;
+/*  uint N;
   while (~scanf("%d", &N)) {
-    for (uint32_t i = 0; i < N; ++i) {
+    for (uint i = 0; i < N; ++i) {
       scanf("%d", prices+i);
     }
-    pair<uint32_t, uint32_t> len_cnt;
+    pair<uint, uint> len_cnt;
     len_cnt = buylowBuylower(N);
     cout << len_cnt.first << "," << len_cnt.second << endl;
   }*/
 
   double prices[] = {10.00,11.50,9.78,15.10,12.48,10.00,13.99,11.72,19.00,18.59};
-  uint32_t n = sizeof(prices)/sizeof(prices[0]);
-  cout << "Max profit with 1 transaction: " << maxProfit(prices, n) << endl;
-  cout << "Max profit with 1 transaction: " << maxProfit4(prices, n, 1) << endl;
-  cout << "Max profit with any transactions: " << maxProfit2(prices, n) << endl;
-  cout << "Max profit with 2 transactions: " << maxProfit3(prices, n) << endl;
-  cout << "Max profit with 2 transactions: " << maxProfit4(prices, n, 2) << endl;
+  uint n = sizeof(prices)/sizeof(prices[0]);
+  cout << "Max profit with 1 transaction: " << maxProfit_1Trans(prices, n) << endl;
+  cout << "Max profit with 1 transaction: " << maxProfit_kTrans(prices, n, 1) << endl;
+  cout << "Max profit with any transactions: " << maxProfit_anyTrans(prices, n) << endl;
+  cout << "Max profit with 2 transactions: " << maxProfit_2Trans(prices, n) << endl;
+  cout << "Max profit with 2 transactions: " << maxProfit4_kTrans(prices, n, 2) << endl;
   double px[] = {10, 22, 5, 75, 65, 80};
   n = sizeof(px)/sizeof(px[0]);
-  cout << "Max profit with at most 2 transactions a day: " << maxProfit5(px, n) << endl;
+  cout << "Max profit with at most 2 transactions a day: " << maxProfit_2TransADay(px, n) << endl;
 
   return 0;
 }
